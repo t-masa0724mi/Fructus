4434686cf0daf8848ec7021170574b5997e31d5a
masa950724mi <s100.iizuka.masami@gmail.com>
2023-06-07 18:18:40 +0900

    リストの追加

--- Fructus/App/ContentView.swift
+++ Fructus/App/ContentView.swift
@@ -1,26 +1,24 @@
-//
-//  ContentView.swift
-//  Fructus
-//
-//  Created by 飯塚政美 on 2023/06/07.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    var fruits: [Fruit] = fruitsData
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(fruits.shuffled()) { item in
+                    FruitRowView(fruits: item)
+                        .padding(.vertical, 4)
+                }
+            }
+            .navigationTitle("Fruits")
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(fruits: fruitsData)
     }
 }
