cff962bd19f80a3cdd100900e0a125559b23bc7e
masa950724mi <s100.iizuka.masami@gmail.com>
2023-06-07 12:29:10 +0900

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by 飯塚政美 on 2023/06/07.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
